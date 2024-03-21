3917f95488daa394f8cfc673470b8361deba8dbc
Hunter_Prash <rajaji.prashant@gmail.com>
2024-03-21 22:02:30 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Greet from './components/Greet';
+import { Status } from './components/Status';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/oscar';
+import { Button } from './components/Button';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Greet name={'Prashant'} mssgCount={10} isLoggedin={true}/>
+        <Status status='error'/>
+        <Heading children={'Hii'}/>
+
+
+        <Oscar>{/*If we want to send a react component as a prop itself Then we need to declare the type in the Oscar tsx file as React.ReactNode*/}
+          <Heading children='hii'/>
+        </Oscar>
+
+        <Button handleClick={(event)=>{
+            console.log('Button clicked',event)
+          }}/>
+          
+        
     </div>
   );
 }
